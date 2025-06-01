b364ee94195698b77ec8a56f64b30b19ed75f504
Andrey Volkov <mycodepg@gmail.com>
2025-06-01 09:34:31 +0300

    hw 2.6

--- src/main/java/course/concurrency/m2_async/spring/AsyncClassTest.java
+++ src/main/java/course/concurrency/m2_async/spring/AsyncClassTest.java
@@ -3,9 +3,11 @@ package course.concurrency.m2_async.spring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
-
+@EnableAsync
 @Component
 public class AsyncClassTest {
 
@@ -16,11 +18,13 @@ public class AsyncClassTest {
     @Qualifier("applicationTaskExecutor")
     private ThreadPoolTaskExecutor executor;
 
+    @Async
     public void runAsyncTask() {
         System.out.println("runAsyncTask: " + Thread.currentThread().getName());
-        internalTask();
+        context.getBean(AsyncClassTest.class).internalTask();
     }
 
+    @Async
     public void internalTask() {
         System.out.println("internalTask: " + Thread.currentThread().getName());
     }
